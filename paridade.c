4481070c08ec39769e71ea28b57065a3a63b0d71
Lavinia Medeiros <laviniameds@icloud.com>
2017-09-12 23:39:50 -0300

    paridade ok mas n roda no SPOJ

--- paridade.c
+++ paridade.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
+
+void binario(int n){
+  int i, j = 0;
+  char bin[32] = {0};
+  while(n != 0){
+    if(n%2 == 0) bin[j] = '0';
+    else bin[j] = '1';
+    n = n/2;
+    j++;
+  }
+  for(i=31; i>=0; i--){
+   printf("%c", bin[i]);
+  }
+}
+
+int paridade(int n){
+  int count = 0;
+  while(n != 0){
+    if(n%2 == 1)
+      count++;
+    n = n/2;
+  }
+  return count;
+}
+
 int main(){
-  int l, count = 0, resto;
-  char *bin;
+  int l;
   scanf("%d", &l);
   while(l != 0){
-    if(l = 1) printf("The parity of %d is %d (mod 2)", 1, 1);
-    else if()
-    while(l / 2 != 0){
-      if(l%2 == 1){
-        count++;
-        bin += "1";
-      }
-      else bin += "0";
-      l = l/2;
-    }
+    printf("The parity of ");
+    binario(l);
+    printf(" is %d (mod 2).\n", paridade(l));
+    scanf("%d", &l);
   }
   return 0;
 }
