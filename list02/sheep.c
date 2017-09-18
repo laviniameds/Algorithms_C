95449ddc1f40d5841453c734616cb8254687b09d
Lavinia Medeiros <laviniameds@icloud.com>
2017-09-18 10:52:31 -0300

    sheep OK

--- list02/sheep.c
+++ list02/sheep.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-  int t, n, i, j, count = 0, aux = 0;
+  int t, n, i, j, count=0;
   scanf("%d", &t);
   while(t != 0){
     scanf("%d", &n);
@@ -10,25 +10,20 @@ int main(){
       scanf("%d", &carneiros[i]);
     }
 
-    for(i=0;i<n-1;i++){
-          for(j=0;j<n-i-1;j++){
-            if(carneiros[j]>carneiros[j+1]){
-              aux = carneiros[j];
-              carneiros[j] = carneiros[j+1];
-              carneiros[j+1] = aux;
-            }
-          }
-        }
-
     for(i=0;i<n;i++){
       for(j=(i+1);j<n;j++){
         if(carneiros[i] == carneiros[j])
-          count++;
+          carneiros[i] = -1;
       }
     }
 
-    printf("%d\n", n-count);
+    for(i=0;i<n;i++){
+      if(carneiros[i] != -1)
+      count++;
+    }
+    printf("%d\n", count);
 
+    count=0;
     t--;
   }
   return 0;
