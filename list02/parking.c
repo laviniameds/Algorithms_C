3282206be1ba411accd4fd31dc79603e2de41014
Lavinia Medeiros <laviniameds@icloud.com>
2017-10-01 22:19:25 -0300

    parking NOT OK

--- list02/parking.c
+++ list02/parking.c
@@ -2,37 +2,39 @@
 
 int main(){
 
-  int n, c, i, p, t, count=0, valor=0, k, x;
-  char e;
-  while(scanf("%d%d", &c, &n) != EOF){
-    printf("eof: %d %d\n", c, n);
+  int n, c, i, p, t, count=0, valor=0, k;
+  char e[2];
+  while(scanf("%d %d", &c, &n) != EOF){
     int estacionamento[c];
 
     for(i=0;i<c;i++)
       estacionamento[i] = 1;
 
-
     for(i=0;i<n;i++){
-      scanf("%c %d", &e, &p);
-      if(e == 'C'){
+      scanf("%s %d", e, &p);
+      if(e[0] == 'C'){
         scanf("%d", &t);
 
         for(k=0;k<c;k++){
           if(estacionamento[k] == 1){
             count++;
             if(count == t){
-              for(x=k;x>=0;x--)
-                estacionamento[x] = p;
+              while(t>=0){
+                estacionamento[k--] = p;
+                t--;
+              }
               valor += 10;
+              count=0;
+              break;
             }
           }
+          else count=0;
         }
       }
       else{
         for(k=0;k<c;k++)
           if(p == estacionamento[k])
             estacionamento[k] = 1;
-        count=0;
       }
     }
     printf("%d\n", valor);
