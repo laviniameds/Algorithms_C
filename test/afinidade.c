baa3e11d64f97f233d96404a0bc27d7a6a66f95a
laviniameds <laviniameds@icloud.com>
2017-11-28 06:24:23 -0300

    afinidade backtracking

--- test/afinidade.c
+++ test/afinidade.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main() {
 
-  int afinidade, qtdPessoas, i, j, k, x, z, p1, p2, p3, maior=0;
+int afinidade, qtdPessoas, i, j, k, x, z, p1, p2, p3, maior=0;
 
   scanf("%d %d" ,&qtdPessoas, &afinidade);
 
   int matriz[afinidade][afinidade];
 
-/*zera matriz*/
+
   for(k=0;k<afinidade;k++)
     for(x=0;x<afinidade;x++)
       matriz[k][x] = 0;
@@ -31,4 +31,43 @@ int main() {
   printf("%d\n%d\n%d\n%d\n", maior, p1, p2, p3);
 
   return 0;
+}*/
+
+#include <stdio.h>
+
+#define MAX 1001
+
+int maior_afinidade(int afinidades[MAX][MAX],int n){
+  int maior_afinidade = 0, i, j, k;
+    for (i=0 ; i<(n-2) ; ++i){
+      for (j=i+1 ; j<(n-1) ; ++j){
+        for (k=j+1 ; k<n ; ++k){
+          if ((afinidades[i][j] + afinidades[i][k]) > maior_afinidade){
+            maior_afinidade = afinidades[i][j] + afinidades[i][k];
+          }
+        }
+      }
+    }
+  return maior_afinidade;
+}
+int main(){
+  int i,n,m,k,x,p1,p2,afinidade;
+  int afinidades[MAX][MAX];
+
+  while (scanf("%d %d",&n,&m),n){
+
+    for(k=0;k<n;k++)
+      for(x=0;x<n;x++)
+        afinidades[k][x] = 0;
+
+    for (i=0 ; i<m ; ++i){
+      scanf("%d %d %d",&p1,&p2,&afinidade);
+      afinidades[p1][p2] = afinidade;
+      afinidades[p2][p1] = afinidade;
+    }
+    printf("%d\n",maior_afinidade(afinidades,m));
+  }
+
+  return 0;
+
 }
